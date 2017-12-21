42a65555c38cef68dd926e2cf6ce3ade1ce29aa4
Joanne Song <joannesong@ac.c4q.nyc>
2017-12-20 19:23:37 -0500

    complete part01

--- app/src/main/java/nyc/c4q/unit4/MainActivity.java
+++ app/src/main/java/nyc/c4q/unit4/MainActivity.java
@@ -1,5 +1,8 @@
 package nyc.c4q.unit4;
 
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MainFragment mainFragment = new MainFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.addToBackStack(null).replace(R.id.fragment_container, mainFragment);
+        fragmentTransaction.commit();
+
     }
 }
---
+++ app/src/main/java/nyc/c4q/unit4/MainFragment.java
@@ -0,0 +1,51 @@
+package nyc.c4q.unit4;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainFragment extends Fragment {
+    private Button button;
+    private Context context;
+
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        button = view.findViewById(R.id.button);
+        context = view.getContext();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DisplayFragment displayFragment = new DisplayFragment();
+                FragmentManager fragmentManager = ((FragmentActivity) context).getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.addToBackStack("DisplayFragment").replace(R.id.fragment_container,displayFragment);
+                fragmentTransaction.commit();
+            }
+        });
+
+        return view;
+    }
+
+}
